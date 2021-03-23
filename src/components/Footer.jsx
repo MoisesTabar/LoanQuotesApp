92060dfecd0ea76f5868740226d15a7f44a7fa5c
Moises <MoisesTabar.013@gmail.com>
2021-03-23 18:12:32 -0400

    Deploying

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <div className="footer-container">
-            <footer>Footer</footer>
+            <footer>Made by Moises Tabar &copy; 2020</footer>
         </div>
 
     )
